af59dc7140e1bbfbea51a2914b0f6426ca006012
Robert S <robscafe333@yahoo.com>
2024-09-25 15:07:43 -0700

    in connection.js file and .env file: changed order of user and password as terminal was giving me error message that password needed to be a string. Defined columns in Category.js. Created gif for readme.md file.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const express = require("express");
+const routes = require("./routes");
+require("dotenv").config();
+const sequelize = require("./config/connection"); // Import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+console.log(typeof process.env.DB_PASSWORD); // Should output 'string'
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,6 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
 });
